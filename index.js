feb248efbc65ffcd1610315d65ed7ae41f818ec1
Franklin Fanya <franklinfanya@gmail.com>
2025-09-28 09:03:43 +0100

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ connectDatabase().then(() => {
 });
 
 app.use(cors({
-  origin: ['https://essence-boutique.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
+  origin: ['https://the-perfume-house.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
